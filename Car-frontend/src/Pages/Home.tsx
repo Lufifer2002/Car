adf6dca05b6af162293a91ea00bc91aa2ee5c2e7
Rashel Thapa <thapa.rashel@gmail.com>
2026-05-29 13:00:16 +0545

    updated

--- Car-frontend/src/Pages/Home.tsx
+++ Car-frontend/src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 import Header from "../Component/header";
 import herooo from "../assets/herooo.png";
 import Footer from "../Component/Footer";
-
+import second from "../assets/second.png";
 function Home() {
     return (
         <div className="bg-[#020202] min-h-screen text-white p-4 md:p-6">
@@ -48,31 +48,120 @@ function Home() {
                 {/* Crafting Section */}
                 <div className="relative">
                     <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent pointer-events-none z-10" />
-                    <section className="relative z-20 pb-20 px-6 text-left">
-                        <h3 className="text-4xl md:text-6xl font-bold mb-2">
-                            Crafting the Future
-                        </h3>
-                        <h3 className="text-4xl md:text-6xl font-bold mb-6">
-                            of Luxury
-                        </h3>
-                        <div className="text-left w-full">
-                            <div className="">
+
+                    <section className="relative z-20 pb-20 px-6">
+
+                        <div className="flex w-full gap-10">
+
+                            {/* LEFT SIDE */}
+                            <div className="flex-1 text-left">
+                                <h3 className="text-4xl md:text-6xl font-bold mb-2">
+                                    Crafting the Future
+                                </h3>
+                                <h3 className="text-4xl md:text-6xl font-bold mb-6">
+                                    of Luxury
+                                </h3>
+
                                 <p className="text-gray-400 text-xs md:text-sm leading-5 mb-1">
                                     Redefining the art of luxury automotive
                                 </p>
                                 <p className="text-gray-400 text-xs md:text-sm leading-5 mb-1">
                                     ownership through trust, craftsmanship, and
                                 </p>
-                                <div className="flex flex-wrap items-center justify-start gap-x-2 gap-y-2">
-                                    <p className="text-gray-400 text-xs md:text-sm leading-5">
-                                        global excellence.
+                                <p className="text-gray-400 text-xs md:text-sm leading-5">
+                                    global excellence.
+                                </p>
+                            </div>
+
+                            {/* RIGHT SIDE */}
+                            <div className="flex-3 flex justify-end">
+                                <div className="w-full md:w-2/3 lg:w-1/2 ">
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 mb-1 text-justify">
+                                        Founder in 2022,LuxAuto was born from a passion for exceptional automobiles
                                     </p>
-                                    <h3 className="text-4xl md:text-6xl font-bold bg-gradient-to-r from-white to-gray-400 bg-clip-text text-transparent whitespace-nowrap">
-                                        Our Story
-                                    </h3>
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 mb-1 text-justify">
+                                        and a vision to create the world's most trusted luxury car marketplace. We
+
+                                    </p>
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 text-justify mb-1">
+                                        recognized the need for a platform that connects discerning buyers with verified
+                                    </p>
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 text-justify">
+                                        dealers offering the finest vehicles.
+                                    </p>
+                                    <br />
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 mb-1 text-justify">
+                                        Today, we're proud to serve client across 15 countries, facilitating, transactions
+                                    </p>
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 text-justify mb-1">
+                                        worth over $200 million and building lasting relationships within the luxury
+                                    </p>
+                                    <p className="text-gray-400 text-xs md:text-sm leading-5 text-justify">
+                                        automotive community
+                                    </p>
+
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="relative">
+
+                            {/* Gradient overlay */}
+                            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black/30 pointer-events-none z-10" />
+
+                            {/* Text */}
+                            <h3 className="relative z-20 text-white text-4xl md:text-6xl font-bold mb-2 text-center">
+                                Our Story
+                            </h3>
+                            <div className="relative z-20 mt-10 w-full max-w-4xl mx-auto h-[500px] overflow-hidden">
+                                <img
+                                    src={second}
+                                    alt="Crafting the Future of Luxury"
+                                    className="w-full h-full object-cover object-bottom opacity-80" />
+                                {/* Overlay */}
+
+
+                                {/* Overlay Container */}
+                                <div className="absolute inset-0 z-30 flex items-end justify-start pb-10 px-6 gap-4 flex-wrap">
+
+                                    {/* Card 1 */}
+                                    <div className="bg-black/40 backdrop-blur-md p-6 rounded-2xl text-center w-40 md:w-52">
+                                        <h2 className="text-3xl md:text-4xl font-bold">15+</h2>
+                                        <p className="text-xs md:text-sm text-gray-300 mt-2">
+                                            Global Presence
+                                        </p>
+                                    </div>
+
+                                    {/* Card 2 */}
+                                    <div className="bg-black/40 backdrop-blur-md p-6 rounded-2xl text-center w-40 md:w-52">
+                                        <h2 className="text-3xl md:text-4xl font-bold">120+</h2>
+                                        <p className="text-xs md:text-sm text-gray-300 mt-2">
+                                            Global Dealers
+                                        </p>
+                                    </div>
+
+                                    {/* Card 3 */}
+                                    <div className="bg-black/40 backdrop-blur-md p-6 rounded-2xl text-center w-40 md:w-52">
+                                        <h2 className="text-3xl md:text-4xl font-bold">15+</h2>
+                                        <p className="text-xs md:text-sm text-gray-300 mt-2">
+                                            Years Experience
+                                        </p>
+                                    </div>
+
+                                    {/* Card 4 */}
+                                    <div className="bg-black/40 backdrop-blur-md p-6 rounded-2xl text-center w-40 md:w-52">
+                                        <h2 className="text-3xl md:text-4xl font-bold">98%</h2>
+                                        <p className="text-xs md:text-sm text-gray-300 mt-2">
+                                            Customer Satisfaction
+                                        </p>
+                                    </div>
+
                                 </div>
                             </div>
                         </div>
+
+
+
                     </section>
                 </div>
                 {/* Footer */}
